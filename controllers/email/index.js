a2f2d31dee46bdad4df8ad91614a96e216f36251
SlytherING <khalfaoui.emnaa@gmail.com>
2021-11-21 15:06:54 +0000

    tiny changes in email

--- controllers/email/index.js
+++ controllers/email/index.js
@@ -1,28 +1,28 @@
+// const creds = require("../../config/contact");
 const nodemailer = require("nodemailer");
 
-const creds = require('../../config/contact');
-//const creds = require("../../config/creds");
+const creds = require("../../config/creds");
 
 var transport = {
-    service: "gmail",
-    secure: false,
-    port: 25,
-    auth: {
-      user: creds.USER,
-      pass: creds.PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
+  service: "gmail",
+  secure: false,
+  port: 25,
+  auth: {
+    user: creds.USER,
+    pass: creds.PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
 };
-  
+
 var transporter = nodemailer.createTransport(transport);
-  
+
 transporter.verify((error, success) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Server is ready to take messages");
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take messages");
+  }
 });
-exports.transporter = transporter
+exports.transporter = transporter;
