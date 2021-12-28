20e86a6d307a0d7ee1009c9ecfe515ba4bf80e77
mariem <mariemhajjem10@gmail.com>
2021-12-28 12:17:04 +0100

    add error code

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
   if (req.headerSent) return next(error);
   res.status(error.code || 500);
-  res.json({ message: error.message || "An unknow error occured!" });
+  res.json({ message: error.message || "An unknow error occured!", code:error.code });
 });
 
 DB();
